25f07ade81f5d3e7a23f51a8ca60df3e05a825a0
Isa a bela <ra49418215802@acaonsfatima.org.br>
2022-05-16 18:10:56 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Oi gemêa Ashley ;)</p>
+        <p> Oi gemêa Ashley P</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
